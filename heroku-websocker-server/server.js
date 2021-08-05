c6928db49c7d1a4f29a660fb9d7339d605080377
toyama satoshi <toyamarinyon@gmail.com>
2021-08-05 13:49:58 +0900

    feat: ディレクトリ構造を調整

---
+++ heroku-websocker-server/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+
+// console.log(`listening port :${PORT}`);
+
+wss.on('connection', function connection(ws) {
+    console.log('connection');
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        wss.clients.forEach((client) => {
+            client.send(message);
+        });
+    });
+
+    ws.on('close', function() {
+        console.log('close');
+    });
+});
+
